afd3e33441ea4ba0bdb79e233c5afb49028757ac
Thomas Hochstein <thh@inter.net>
2010-02-25 19:32:51 +0100

    Release 0.6
    Add changelog entries.
    Bump version number.
    
    Signed-off-by: Thomas Hochstein <thh@inter.net>

--- yapfaq.pl
+++ yapfaq.pl
@@ -12,7 +12,7 @@
 # It can be redistributed and/or modified under the same terms under 
 # which Perl itself is published.
 
-my $Version = "0.6-unreleased";
+my $Version = "0.6";
 
 my $NNTPServer = "localhost";
 my $NNTPUser = "";
